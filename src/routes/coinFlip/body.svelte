1bdb7696d7174d58833d855426519bf8075d2f8f
Dida <dimitrije.despotovski@yahoo.com>
2025-07-07 18:00:10 +0200

    Finished animations for coin flip
    Added anime.js and did all animation work with it

--- src/routes/coinFlip/body.svelte
+++ src/routes/coinFlip/body.svelte
@@ -1,5 +1,7 @@
 <script lang="ts">
     import {fade} from 'svelte/transition'
+    import {createTimeline} from 'animejs';
+    import {onMount} from "svelte";
 
     let minimum:number = $state(1);
     let max:number = $state(100);
@@ -7,7 +9,41 @@
         let value:number = $state(max/2);
     let moneyWon:any = $state(null);
     let heads:boolean = $state(false);
-	
+	let coinString:string = $state("");
+
+ 
+    let timeline = createTimeline({ defaults: { duration: 5000 } });
+    let divElement:any;
+    let moneyWonAnimation:any;
+    let coinStringAnimation:any;
+   onMount(() => {
+    timeline
+    .label('start')
+    .label('middle',2000)
+    .label('end')
+    .add(divElement,{
+        rotateY: '720deg',
+      duration: 2000,
+      easing: 'easeInOutSine',      // Animation for coin flip
+      autoplay: false,
+      loop:false
+    },'start')
+    .add(coinStringAnimation,{
+        opacity:{from:0},
+        duration:2000,
+        easing:'easeInOutSine',     //Animation for text on the coin
+        autoplay:false,
+        loop:false
+    })
+    .add(moneyWonAnimation,{
+        opacity:{from:0},
+        duration:3000,
+        easing:'easeInOutSine',     //Last animation if won or lost 
+        autoplay:false,
+        loop:false
+    },'end')
+
+  });
 
     function flipCoin(){
         return Math.round(Math.random());
@@ -30,6 +66,10 @@
         height: 20vh;
         background-color: #BB7333;
         border-radius: 50%;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        font-size: 5vh !important; 
     }
     #slider{
         display: flex;
@@ -59,10 +99,10 @@
 </style>
 <main>   
     <div id = "mainContainer" transition:fade={{duration:1000}}>
-        <div id="coin">
-
+        <div id="coin" bind:this={divElement}>
+            <div bind:this={coinStringAnimation}>{coinString}</div>
         </div>
-        <div id="moneyWon">{moneyWon}</div>
+        <div id="moneyWon" bind:this={moneyWonAnimation}>{moneyWon}</div>
         <div id="slider">
             <p>{value}</p>
             <input type="range" min="{minimum}" max="{max}" bind:value="{value}" id="sliderRange">
@@ -85,8 +125,18 @@
             
             {/if}
         </div>
-        <button id="flipButton" onclick="{()=>{
-            if((flipCoin() == 1 && heads) || (flipCoin() == 0 && !heads)){
+        <button id="flipButton" onclick="{async ()=>{
+            coinString = ""; // every flip coinString resets to empty string
+            const flipValue:number = flipCoin();
+            timeline.restart();
+
+            if(flipValue == 1){
+                coinString = "Heads"
+            }
+            else{
+                coinString = "Tails"
+            }
+            if((flipValue == 1 && heads) || (flipValue == 0 && !heads)){
                 moneyWon = "You won " + value;
             }
             else{
